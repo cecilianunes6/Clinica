3a08fac2485c64656ffbce54ef1076ded3d04c25
jhonatan2104 <xdjhonatan0@gmail.com>
2019-08-28 16:12:01 -0300

    ADD BASE

---
+++ fileTest.py
@@ -0,0 +1,37 @@
+import fileClass as fc
+
+day = fc.Data_day('25','2')
+
+#criando horas para test
+for id_hours in range(5,10,2):
+    day.add_hours_reserved(fc.Data_hours(f'{id_hours}', None))
+for id_hours in range(0,8):
+    day.add_hours_reserved(fc.Data_hours(f'{id_hours}', None))
+
+day.print_day()
+day.sort_hours_reserved() #Teste de ordenação
+day.print_day()
+
+#Teste get_hours_reserved_by_id()
+for h in day.get_hours_reserved_by_id('7'):
+    print(h, end=' ')
+
+print()
+
+#Teste get_hours_reserved_by_interval()
+for h in day.get_hours_reserved_by_interval('5', '7'):
+    print(h, end=' ')
+
+print()
+
+#Teste day
+print(day.name)
+
+print("\n\n")
+
+month = fc.Date_month('3')
+
+for days in range(1, 30, 7):
+    month.add_day(fc.Data_day(f'{days}','2'))
+
+print(month)
